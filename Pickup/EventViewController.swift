1f48c25b69b397cc063064ec039a9216e4e2c044
Jordan Altaffer <Jordan@cpe-2606-a000-bec7-4600-180d-ec47-c562-d1cd.dyn6.twc.com>
2018-05-11 13:45:48 -0400

    something weird happened, file EventViewController.swift deleted, re-added now, functional.

---
+++ Pickup/EventViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Pickup
+//
+//  Created by Jordan Altaffer on 5/7/18.
+//  Copyright © 2018 Jordan Altaffer. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class EventViewController: UITabBarController {
+    //MARK: Properties
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // [START auth_listener]
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            //
+        }
+        // [END auth_listener]
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // [START remove_auth_listener]
+        Auth.auth().removeStateDidChangeListener(handle!)
+        // [END remove_auth_listener]
+    }
+
+    
+    //MARK: Actions
+}
